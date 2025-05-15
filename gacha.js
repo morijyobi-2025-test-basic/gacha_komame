1aeb625bee2c0de86058ba646f7b9cfd687e4bdc
komame <h.komame.sys24@morijyobi.ac.jp>
2025-05-15 12:55:10 +0900

    gachaのリファクタリング
    見やすくなった

--- gacha.js
+++ gacha.js
@@ -1,19 +1,17 @@
 // 割合の定数を読み込む
 import {rs, ra, rb, rc } from './settings.js'
 
+const pool = [
+  ...Array(rs).fill('S'), // Sランクをrs個
+  ...Array(ra).fill('A'), // Aランクをra個
+  ...Array(rb).fill('B'), // Bランクをrb個
+  ...Array(rc).fill('C')  // Cランクをrc個
+]
+
+// pool配列からランダムに1つ選んで返す関数
 const gacha = () => {
-  // S, A, B, Cのいずれかをランダムに返す
-  // Sはrs, Aはra, Bはrb, Cはrc の確率で出る
-  const random = Math.random()*100
-  if (random < rs) {
-    return 'S'
-  } else if (random < rs + ra) {
-    return 'A'
-  } else if (random < rs + ra + rb) {
-    return 'B'
-  } else if (random < rs + ra + rb + rc) {
-    return 'C'
-  }
+  const index = Math.floor(Math.random() * pool.length) // 0以上pool.length未満の整数をランダムに生成
+  return pool[index] // ランダムに選ばれたランクを返す
 }
 
 export default gacha
