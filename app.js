4e070ad252f95fd15f37233bb0c92a8a3d0ceee6
komame <h.komame.sys24@morijyobi.ac.jp>
2025-06-11 16:00:33 +0900

    express
    WebのAPI

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import gacha from "./gacha.js";
+
+const app = express();
+const port = 3000;
+
+app.get("/gacha", (req, res) => {
+  res.json({
+    result: gacha(),
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
